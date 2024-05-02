ce570d5eba3e28db1d9cec2d518de170cc1f699a
Sergio Diaz <sadiazcaballero@gmail.com>
2024-05-02 17:21:24 +0200

    WD-FT-APRIL24-Diaz Lab done 02.05.2024

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
+// import { useState } from "react";
+
 import "./App.css";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import StudentDetailsPage from "./pages/StudentDetailsPage";
 import UserProfilePage from "./pages/UserProfilePage";
-
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
     <div className="App relative z-20 pt-20">
       <Navbar />
 
       <div className="pages">
-        <HomePage />
-
-        <StudentDetailsPage />
-
-        <UserProfilePage />
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route
+            path="/students/:studentId"
+            element={<StudentDetailsPage />}
+          ></Route>
+          <Route path="/profile" element={<UserProfilePage />}></Route>
+        </Routes>
       </div>
-
     </div>
   );
 }
